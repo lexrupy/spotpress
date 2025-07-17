39b81e45cc43c73a8b8793cc68a5e7c3fd48b7c6
Alexandre da Silva <lexrupy@gmail.com>
2025-07-17 09:07:53 -0300

    better naming generation

--- build-exe.bat
+++ build-exe.bat
@@ -12,11 +12,14 @@ IF EXIST build (
 )
 REM Compila o script com PyInstaller
 pyinstaller ./main.py --icon=spotpress.ico --noconsole --add-data "spotpress.png;."
+pyinstaller ./spotpressctl.py --icon=spotpress.ico
 
 REM Verifica se o build foi bem-sucedido
 IF EXIST ".\dist\main\main.exe" (
-    echo Renaming main.exe to spotpress.exe
-    move /Y ".\dist\main\main.exe" ".\dist\main\spotpress.exe"
+    echo Reorganizing files
+    move /Y ".\dist\main" ".\dist\spotpress"
+    move /Y ".\dist\spotpress\main.exe" ".\dist\spotpress\spotpress.exe"
+    move /Y ".\dist\spotpressctl\spotpressctl.exe" ".\dist\spotpress\spotpressctl.exe"
 ) ELSE (
     echo ERROR: File main.exe not found!
 )
